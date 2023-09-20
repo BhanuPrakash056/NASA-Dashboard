07e1ff2ad3c7e48f9ff253104d3e2d948ea56f1e
bhanu prakash <r19cs056@cit.reva.edu.in>
2023-09-20 19:10:50 +0530

    added morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router");
-
+const morgan = require("morgan");
 const app = express();
+app.use(morgan('combined'))
 app.use(
   cors({
     origin: "http://localhost:3000",
