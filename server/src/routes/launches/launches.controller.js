3f37b2ecfbb7a0d156b2cfbff07f8b53eb13b594
bhanu prakash <r19cs056@cit.reva.edu.in>
2023-10-03 20:54:07 +0530

    added a delete requet

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,6 +1,8 @@
 const { 
   getAllLaunches,
   addNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById,
 } = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
@@ -11,7 +13,7 @@ function httpAddNewLaunch(req, res) {
   const launch = req.body;
 
   if (!launch.mission || !launch.rocket || !launch.launchDate
-    || !launch.destination) {
+    || !launch.target) {
       return res.status(400).json({
         error: 'Missing required launch property',
       });
@@ -28,7 +30,21 @@ function httpAddNewLaunch(req, res) {
   return res.status(201).json(launch);
 }
 
+function httpAbortLaunch(req, res) {
+  const launchId = Number(req.params.id);
+
+  if (!existsLaunchWithId(launchId)) {
+    return res.status(404).json({
+      error: 'Launch not found',
+    });
+  }
+
+  const aborted = abortLaunchById(launchId);
+  return res.status(200).json(aborted);
+}
+
 module.exports = {
   httpGetAllLaunches,
   httpAddNewLaunch,
+  httpAbortLaunch,
 };
